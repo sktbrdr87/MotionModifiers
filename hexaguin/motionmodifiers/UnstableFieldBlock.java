1f10ca5a416a1206fc2caa56f22b5619f4298009
hexaguin <hexaguin@gmail.com>
2013-05-01 12:11:52 -0600

    Included textures and changed directory again
    This now goes in <FORGE FOLDER>/mcp/src/.

--- hexaguin/motionmodifiers/UnstableFieldBlock.java
+++ hexaguin/motionmodifiers/UnstableFieldBlock.java
@@ -2,10 +2,12 @@ package hexaguin.motionmodifiers;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.item.EntityTNTPrimed;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
 
 public class UnstableFieldBlock extends Block {
@@ -22,6 +24,19 @@ public class UnstableFieldBlock extends Block {
 		par5Entity.posZ = Math.random()*1000;
     }
 	
-	
+	public boolean isOpaqueCube()
+    {
+        return false;
+    }
+
+	public AxisAlignedBB getCollisionBoundingBoxFromPool(World par1World, int par2, int par3, int par4)
+    {
+        return null;
+    }
+
+    
+    public void registerIcons(IconRegister iconRegister){
+        blockIcon = iconRegister.registerIcon("MotionModifiers:UnstableField");
+	}
 
 }
